6710d5a7d636c2eadcf9cf0902dde131d9187e0b
AleksandraS22243 <127045556+AleksandraS22243@users.noreply.github.com>
2024-05-26 15:50:15 +0200

    adding cw6

--- cw6/MediApp/MediApp/Startup.cs
+++ cw6/MediApp/MediApp/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 namespace MediApp
 {
@@ -21,6 +24,22 @@ namespace MediApp
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MediApp API",
+                    Description = "A simple example ASP.NET Core Web API for managing medical prescriptions",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -33,6 +52,16 @@ namespace MediApp
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
+
+
+            app.UseSwagger();
+
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MediApp API V1");
+                c.RoutePrefix = string.Empty; 
+            });
 
             app.UseEndpoints(endpoints =>
             {
